7eb3dfca2855a9a7d722e6df22f458a9bfd9fdd9
proazad <proazadhossain7595@gmail.com>
2023-12-15 13:39:37 +0300

    Navbar Updates

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,26 +1,59 @@
 import logo from "@/assets/image/logo.png";
+import { Instagram, LinkedIn, Twitter } from "@mui/icons-material";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { IconButton, Stack } from "@mui/material";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
 import Toolbar from '@mui/material/Toolbar';
 import Image from 'next/image';
-const pages = ['Products', 'Pricing', 'Blog'];
+import Link from "next/link";
+const navlinks = [
+    { router: "Home", routerPath: "/" },
+    { router: "Pages", routerPath: "/pages" },
+    { router: "Category", routerPath: "/category" },
+    { router: "News", routerPath: "/news" },
+    { router: "Post", routerPath: "/post" },
+    { router: "Contact", routerPath: "/contact" },
+];
 const Navbar = () => {
     return (
         <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Image src={logo} alt='Logo' width={100} height={100}/>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
+            <Container maxWidth="xl" className="bg-gray-900">
+                <Toolbar disableGutters >
+                    <Image src={logo} alt='Logo' width={100} height={100} />
+                    <Box className="w-full text-center">
+                        {navlinks.map((item) => (
+                            <Link key={item} href={item.routerPath}>
+                                <Button
+                                    className="text-white"
+                                >
+                                    {item.router}
+                                </Button>
+                            </Link>
                         ))}
+                    </Box>
+                    <Box sx={{ "& svg": { color: "white" } }}>
+                        <Stack
+                            direction="row"
+                            divider={<Divider orientation="vertical" flexItem />}
+                            spacing={1}
+                        >
+                            <IconButton color="white" aria-label="Facebook">
+                                <FacebookIcon />
+                            </IconButton>
+                            <IconButton color="white" aria-label="Twitter">
+                                <Twitter />
+                            </IconButton>
+                            <IconButton color="white" aria-label="Instagram">
+                                <Instagram />
+                            </IconButton>
+                            <IconButton color="white" aria-label="Linkedin">
+                                <LinkedIn />
+                            </IconButton>
+                        </Stack>
                     </Box>
                 </Toolbar>
             </Container>
